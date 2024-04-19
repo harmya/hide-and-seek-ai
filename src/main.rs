6a02e25077f47922cf814a245d92b38222fc29f6
harmya <hvbhatt@purdue.edu>
2024-04-18 23:00:17 -0400

    window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use macroquad::rand::gen_range;
+use macroquad::{prelude::*};
+
+
+
+#[macroquad::main(window_conf)]
+async fn main() {
     println!("Hello, world!");
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Hide and Seek".to_owned(),
+        window_width: 800,
+        window_height: 600,
+        ..Default::default()
+    }
 }
